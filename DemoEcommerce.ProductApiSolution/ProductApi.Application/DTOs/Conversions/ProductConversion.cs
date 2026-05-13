1444c0bada8cf6ea6098e3525023c23d01dae601
vitor <lageduque@gmail.com>
2026-05-13 14:52:54 -0400

    Update Product DTO conversion methods

--- DemoEcommerce.ProductApiSolution/ProductApi.Application/DTOs/Conversions/ProductConversion.cs
+++ DemoEcommerce.ProductApiSolution/ProductApi.Application/DTOs/Conversions/ProductConversion.cs
@@ -16,13 +16,13 @@ namespace ProductApi.Application.DTOs.Conversions
         public static (ProductDTO?, IEnumerable<ProductDTO>?) FromEntity(Product product, IEnumerable<Product>? products)
         {
             // return single
-            if (product is not null || products is null)
+            if (product is not null && products is null)
             {
                 var singleProduct = new ProductDTO(product!.Id, product.Name!, product.Quantity, product.Price);
                 return (singleProduct, null);
             }
 
-            if (products is not null || product is null)
+            if (products is not null && product is null)
             {
                 var _products = products!.Select(p =>
                 new ProductDTO(p.Id, p.Name!, p.Quantity, p.Price)).ToList();
