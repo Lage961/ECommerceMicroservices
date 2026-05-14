6482bc842610ed6106d62cb71d6bc0c9559170fb
vitor <lageduque@gmail.com>
2026-05-14 03:16:24 -0400

    Implemented Dependecy Injection Functionality

---
+++ DemoEcommerce.ProductApiSolution/ProductApi.Infrastructure/DependencyInjection/ServiceContainer.cs
@@ -0,0 +1,33 @@
+﻿
+using eCommerce.SharedLibrary.DependencyInjection;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using ProductApi.Application.Interfaces;
+using ProductApi.Infrastructure.Data;
+using ProductApi.Infrastructure.Repositories;
+
+namespace ProductApi.Infrastructure.DependencyInjection
+{
+    public static class ServiceContainer
+    {
+        public static IServiceCollection AddInfrastructureService(this IServiceCollection services, IConfiguration configuration)
+        {
+            //Add authentication Connectivity
+            SharedServiceContainer.AddSharedServices<ProductDbContext>(services, configuration, configuration["MySerilog:FineName"]!);
+            //Add authentication Scheme
+            services.AddScoped<IProduct, ProductRepository>();
+
+            return services;
+        }
+
+        public static IApplicationBuilder UseInfrastructurePolicy(this IApplicationBuilder app)
+        {
+            //Register middleware such as:
+            // Global Excpetion: handles external errors
+            // Listen to Only Api Gateway: Blocks all outsider calls
+            SharedServiceContainer.UsedSharedPolicies(app);
+            return app;
+        }
+    }
+}
